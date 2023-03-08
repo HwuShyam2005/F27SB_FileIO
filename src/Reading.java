0fa1ce8a3029b3f10d84d4a4e7b15937081e0063
user <user@SHYAM.du.hw.ac.uk>
2023-03-08 09:38:10 +0400

    FINAL CODE

--- src/Reading.java
+++ src/Reading.java
@@ -1,9 +1,48 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 public class Reading {
-
+	// File path for conversation 
+	private static final String FILE_PATH = "conversation.txt";
+	//Method to read with Scanner
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Calling the methods
+		Reading.readWithScanner();                                                               //READING OF SCANNER
+		System.out.print(System.lineSeparator());                                                //LINE SEPERATOR
+	    Reading.readWithBufferedReader();                                                        //READING WITH BUFFEREDREADER
+	}
+	public static void readWithScanner() {
+		try (Scanner scanner = new Scanner(new FileReader(FILE_PATH))) {                         // IMPLEMENTATION OF SCANNER
+			// To check if the file has a next line
+			while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());                                          //CODE HELPS TO FIND NEXT LINE 
+            }
+		}
+		// To throw an exception for accessing the file.
+		catch (IOException e) {
+            System.err.println("Error in reading file: " + e.getMessage());
+        }
+	}
+	// Method to read with buffered reader                                                       //CATCHING THE ERROR WHILE DOING TRY AND
+	public static void readWithBufferedReader() {                                                //CATCH METHOD WHILE USING BUFFEREADER
+		try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+			//Variable for the line 
+			String textLine;
+			//Variable to keep track of the line count which is set to 0 initially.
+			int textLineCount = 0;
+			while((textLine = reader.readLine()) != null) {
+				textLineCount++;
+                System.out.println(textLine);
+				if (textLineCount % 2 == 0) {
+                    System.out.print("Press Enter to continue...");
+                    new Scanner(System.in).nextLine();
+                }
+			}
+		}
+		catch (IOException e) {
+            System.err.println("Error in reading file: " + e.getMessage());
+        }
 	}
 
 }
--- src/Writing.java
+++ src/Writing.java
@@ -1,8 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Writing {
+	private String folder;
+	
+	public Writing(String folder) {
+        this.folder = folder;
+    }
+	
+	public void write(String text) {                                                 
+        try {
+            BufferedWriter writing = new BufferedWriter(new FileWriter(folder));
+            writing.write(text);
+            writing.close();
+        } 
+        // To throw an exception for accessing the file.
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {                                         //THE STRING TEXT THAT MUST BE PRINTED AS OUTPUT FROM CONVERSATION.TXT
+		// Text is the variable for all the text.
+		 String text = "What took you so long?\n" +
+	                "Well, you know, Master, I couldn't find a speeder that I really liked...\n" +
+	                "There he is.\n" +
+	                "...with an open cockpit and the right speed capabilities.\n" +
+	                "If you spent as much time practicing your saber techniques as you did your wit,\n" +
+	                "you'd rival Master Yoda as a swordsman.\n" +
+	                "I thought I already did.\n" +
+	                "Only in your mind, my very young apprentice.";
+		 Writing writer = new Writing("conversation.txt");
+	     writer.write(text);
 
 	}
 
